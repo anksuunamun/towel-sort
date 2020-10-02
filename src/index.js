f8b73640783c769866226cd83e99d3ee88244e5e
anksuunamun <shishkina_veronika_10@mail.ru>
2020-10-03 01:10:56 +0300

    feat: add desicion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  
-  return [];
+module.exports = function towelSort(matrix = []) {
+
+    let unsortedArray = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (Array.isArray(matrix[i])) {
+            let m = [];
+            for (let elem of matrix[i]) {
+                m.push(elem);
+            }
+            if (i % 2 === 0) {
+                for (let item of m) {
+                    unsortedArray.push(item)
+                }
+            } else {
+                for (let item of m.reverse()) {
+                    unsortedArray.push(item)
+                }
+            }
+        }
+    }
+
+
+
+
+    return unsortedArray;
 }
